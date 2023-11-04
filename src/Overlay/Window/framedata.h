9d7e0a548be6010aba3b9ec58cc1b4668fb495b3
PC_volt <PC_volt@DESKTOP-I20R60L>
2023-11-04 18:45:08 +0100

    Accounting for the non actionable 1F landing post ukemi

--- src/Overlay/Window/framedata.h
+++ src/Overlay/Window/framedata.h
@@ -12,7 +12,7 @@ struct PlayersInteractionState
 {
 	// World Time
 	int prevFrameCount = 0;
-	
+
 	// Frame advantage
 	int timer = 0;
 	int frameAdvantageToDisplay = 0;
@@ -26,14 +26,34 @@ struct PlayersInteractionState
 	int p2GapDisplay = -1;
 };
 
+struct CharDataExtended
+{
+	CharDataExtended()
+	{
+		this->charData = charData;
+		this->previousAction = "";
+	};
+
+	void updatePreviousAction()
+	{
+		const std::string currentActionString = charData->currentAction;
+		previousAction = currentActionString;
+	}
+
+	CharData* charData;
+	std::string previousAction;
+};
+
 extern IdleToggles idleToggles;
 extern PlayersInteractionState playersInteraction;
+extern CharDataExtended player1;
+extern CharDataExtended player2;
 
 bool isDoingActionInList(const char currentAction[], const std::list<std::string>& listOfActions);
-bool isIdle(CharData& player);
-bool isBlocking(CharData& player);
-bool isInHitstun(CharData& player);
-void getFrameAdvantage(CharData& player1, CharData& player2);
-void computeGaps(CharData& player, int& gapCounter, int& gapResult);
+bool isIdle(const CharDataExtended& player);
+bool isBlocking(const CharDataExtended& player);
+bool isInHitstun(const CharDataExtended& player);
+void getFrameAdvantage(const CharDataExtended& player1, const CharDataExtended& player2);
+void computeGaps(const CharDataExtended& player, int& gapCounter, int& gapResult);
 bool hasWorldTimeMoved();
 void computeFramedataInteractions();
